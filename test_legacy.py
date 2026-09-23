868f0f8f0ce5c2c37a98f3e790f970967fb712e9
git <davit.ners02@gmail.com>
2026-09-23 11:36:30 +0200

    add exo 2 correction

--- test_legacy.py
+++ test_legacy.py
@@ -7,3 +7,19 @@ Lancez : pytest test_legacy.py -v
 from legacy_pricing import loyalty_discount_percent, price_with_loyalty
 
 # À vous d'écrire les tests.
+def test_low_loyalty_discount_percent():
+    assert loyalty_discount_percent(0) == 0
+    assert loyalty_discount_percent(2) == 0
+
+def test_medium_loyalty_discount_percent():
+    assert loyalty_discount_percent(3) == 5
+    assert loyalty_discount_percent(5) == 5
+    assert loyalty_discount_percent(9) == 5
+
+def test_high_loyalty_discount_percent():
+    assert loyalty_discount_percent(10) == 10
+    assert loyalty_discount_percent(11) == 10
+
+def test_price_with_loyalty():
+    assert price_with_loyalty(100, 10) == 90
+
